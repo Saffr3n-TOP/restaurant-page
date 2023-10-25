b3b4127dca40260dd48c2f81d99f4928491c1f4b
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-25 12:12:11 +0300

    Create main component

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// @ts-check
+import '../assets/styles/main.css';
+import home from './home';
+
+const main = document.createElement('main');
+main.className = 'main';
+main.appendChild(home);
+
+export default main;
