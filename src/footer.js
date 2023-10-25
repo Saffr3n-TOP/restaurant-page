a0815233182378d281aafc2589fed16af075fef6
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-25 12:13:48 +0300

    Create footer component

---
+++ src/footer.js
@@ -0,0 +1,8 @@
+// @ts-check
+import '../assets/styles/footer.css';
+
+const footer = document.createElement('footer');
+footer.className = 'footer';
+footer.textContent = 'No less awesome footer, that serves no real purpose.';
+
+export default footer;
