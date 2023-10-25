2a6e39630881759ff24e985ed8e67d6bdb68060f
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-25 12:09:43 +0300

    Create logo component

---
+++ src/logo.js
@@ -0,0 +1,8 @@
+// @ts-check
+import '../assets/styles/logo.css';
+
+const logo = document.createElement('div');
+logo.className = 'logo';
+logo.textContent = 'Awesome Restaurant';
+
+export default logo;
